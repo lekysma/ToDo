81b4a485d47fb05dc71df60c080b67c459a88adf
lekysma <jeanmartinkyssama@yahoo.com>
2019-09-28 10:27:32 +0000

    conversion au MVC + ajout de classe pour mieux utiliser nos elements

--- ToDo/Controllers/ItemViewController.swift
+++ ToDo/Controllers/ItemViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ItemViewController: UITableViewController {
-    var itemArray = ["Genese", "Exode", "Levitique"]
+    var itemArray = [Item]()
     
     // user defaults
     let defaults = UserDefaults.standard
@@ -18,10 +18,24 @@ class ItemViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        let item1 = Item()
+        item1.title = "Genese"
+        itemArray.append(item1)
+        
+        let item2 = Item()
+        item2.title = "Exode"
+        itemArray.append(item2)
+        
+        let item3 = Item()
+        item3.title = "Levitique"
+        itemArray.append(item3)
+        
         // on charge les donnees sauvegardees dans user defaults
-        if let items = defaults.array(forKey: "ItemList") as? [String] {
+        if let items = defaults.array(forKey: "ItemList") as? [Item] {
             itemArray = items
         }
+
+        
     }
     
     //MARK: - Table view data source
@@ -34,7 +48,14 @@ class ItemViewController: UITableViewController {
         
         let indexPathRow = itemArray[indexPath.row]
         
-        cell.textLabel?.text = indexPathRow
+        cell.textLabel?.text = indexPathRow.title
+        
+        // en fonction de la propriete 'done' on affiche ou pas le 'checkmark'
+        if indexPathRow.done == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         
         //cell.accessoryType = .none
         
@@ -46,15 +67,12 @@ class ItemViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        // print(itemArray[indexPath.row])
         
-        //coche cellule selectionee
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        // on change la propriete 'done' de l'element chaque fois qu'on clique sur celui-ci
+        itemArray[indexPath.row].done.toggle()
+        // et on recharge la table pour prendre en compte le changement de propriete
+        tableView.reloadData()
+     // petite animation sympa
         tableView.deselectRow(at: indexPath, animated: true)
-        
         
     }
     
@@ -66,7 +84,10 @@ class ItemViewController: UITableViewController {
         let alert = UIAlertController(title: "Nouvel élément", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Ajouter", style: .default) { (alertAction) in
             // ce qui se passe quand on clique sur 'Ajouter'
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             // on sauvegarde dans user defaults
             self.defaults.set(self.itemArray, forKey: "ItemList")
 //            //on recharge la table pour afficher le nouvel ajout
---
+++ ToDo/Data Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDo
+//
+//  Created by Jean martin Kyssama on 28/09/2019.
+//  Copyright © 2019 Jean martin Kyssama. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
